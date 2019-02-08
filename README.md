503f4243c2502e705b8f57b61dc79d69d70e72fe
Max Strübing <mxstrbng@gmail.com>
2019-02-08 14:01:00 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+You can find the different decks in the `./decks` directory.
+
+USAGE:
+```
+npm run <start|build|pdf|image> <deckname>
+```
---
+++ theme.js
@@ -0,0 +1,28 @@
+import theme from 'mdx-deck/themes'
+import okaidia from 'react-syntax-highlighter/styles/prism/okaidia'
+
+export default {
+  ...theme,
+
+  css: {
+    ...theme.css,
+    backgroundImage: 'url(./background.svg)',
+    backgroundPosition: 'top right',
+    backgroundRepeat: 'no-repeat'
+  },
+
+  colors: {
+    ...theme.colors,
+    text: 'rgb(61, 77, 101)'
+  },
+
+  prism: {
+    style: okaidia
+  }
+
+  // Customize your presentation theme here.
+  //
+  // Read the docs for more info:
+  // https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md
+  // https://github.com/jxnblk/mdx-deck/blob/master/docs/themes.md
+}
