ab47e9727a0620f24badc88c114445549144388c
Max Strübing <mxstrbng@gmail.com>
2019-04-21 13:51:54 +0200

    Update mdx-deck to 2.3.0 (#26)
    - add /build to the .gitignore to reduce the risk to merge the built into the repository
    - change create_decks and pre-commit to just use find inside our deck folder
    - add react-syntax-highlighter and @mdx-deck/themes to package.json because since mdx-deck 2.0 both dependencies are not built-in anymore

--- theme.js
+++ theme.js
@@ -1,12 +1,12 @@
-import theme from 'mdx-deck/themes'
-import okaidia from 'react-syntax-highlighter/styles/prism/okaidia'
+import theme, {syntaxHighlighterPrism} from '@mdx-deck/themes'
+import okaidia from 'react-syntax-highlighter/dist/esm/styles/prism/okaidia'
 
-export default {
+export default syntaxHighlighterPrism({
   ...theme,
 
   css: {
     ...theme.css,
-    backgroundImage: 'url(../images/background.svg)',
+    backgroundImage: 'url(../decks/images/background.svg)',
     backgroundPosition: 'top right',
     backgroundRepeat: 'no-repeat'
   },
@@ -19,10 +19,4 @@ export default {
   prism: {
     style: okaidia
   }
-
-  // Customize your presentation theme here.
-  //
-  // Read the docs for more info:
-  // https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md
-  // https://github.com/jxnblk/mdx-deck/blob/master/docs/themes.md
-}
+});
